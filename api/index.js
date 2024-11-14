448e9bc98fc4a0cf516796f34b5066d48e51fb97
KRITHIKus <krithikus2004@gmail.com>
2024-11-14 19:15:24 +0530

    create a test API route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
+import userRoutes from './routes/user.route.js'
 
 dotenv.config()
 mongoose.
@@ -20,3 +20,5 @@ app.listen(3000,()=>{
 }
 
 )
+
+app.use('/api/user',userRoutes)
