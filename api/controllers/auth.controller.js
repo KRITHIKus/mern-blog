c8531f2e198b687717134f67b43da9b2bbe3fc11
KRITHIKus <krithikus2004@gmail.com>
2024-11-19 06:20:13 +0530

    signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) =>{
 const {username, email, password}= req.body;
@@ -24,5 +25,37 @@ res.json("signup Successfull")
 } catch (error){
    next(error);
 }
+
+};
+
+export const signin= async (req, res, next)=>{
+  const {username, email, password}= req.body;
+  if(!username || !email || !password || username === '' || email ==='' || password === '') {
+  return  next(errorHandler(400,'All fileds are required'))
+  }
+  try{
+    const  validUser= await User.findOne({email})
+    if(!validUser){
+    return  next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword){
+    return  next(errorHandler(401,'Incorrect password'))
+    }
+
+    const token = jwt.sign(
+      { id: validUser._id}, process.env.JWT_SEC );
+
+const {password: pass, ...rest} = validUser._doc;
+
+      res.status(200).cookie('access_token',token,{
+        httpOnly: true,
+      }).json(rest)
+
+  } catch(error){
+    next(error)
+  }
+
+
 
 };
