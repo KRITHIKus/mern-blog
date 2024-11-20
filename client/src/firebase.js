68aeda1c158dc40d076c27a8e1be5f29c1e5ae01
KRITHIKus <krithikus2004@gmail.com>
2024-11-20 16:00:42 +0530

    Oauth functionality and signin with google(firebase)

---
+++ client/src/firebase.js
@@ -0,0 +1,21 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+console.log(import.meta.env.VITE_FIREBASE_API_KEY)
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-blog-8c56f.firebaseapp.com",
+  projectId: "mern-blog-8c56f",
+  storageBucket: "mern-blog-8c56f.firebasestorage.app",
+  messagingSenderId: "466583762858",
+  appId: "1:466583762858:web:4b1f43845273deb0a61dd3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
