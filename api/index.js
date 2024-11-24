ac293005f82605b81353600d7692225550b0f254
KRITHIKus <krithikus2004@gmail.com>
2024-11-24 15:09:35 +0530

    create post api route BE

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.routes.js'
 import cookieParser from 'cookie-parser'
+import PostRoutes from './routes/post.route.js'
+
 
 dotenv.config()
 mongoose.
@@ -27,6 +29,7 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
+app.use('/api/post',PostRoutes)
 
 
 
